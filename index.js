43ce0d14ed145bb61bf47ab1ff483a8839f96f6b
Venus Ang <venus@users.noreply.github.com>
2022-08-14 19:20:29 -0700

    Things that were done: - updated html removed hard coded classes - added new selected and incorrect and correct classes for labels and messages - updated javascript to swap out class names in answer div so new css will kick-in - started updating .scss but you get the idea

--- index.js
+++ index.js
@@ -6,27 +6,53 @@ console.log("radios is not an array");
 console.log("radios".radios);
 
 const radioMessages = {
-    answer1: "That’s not right",
-    answer2: "That’s right!",
-    answer3: "That's not right",
-    answer4: "That's not right"
+    answer1: {
+        correct: false,
+        message: "That's not right",
+    },
+    answer2: {
+        correct: true,
+        message: "That's right!",
+    },
+    answer3: {
+        correct: false,
+        message: "That's not right",
+    },
+    answer4: {
+        correct: false,
+        message: "That's not right"
+    }
 };
 
 radios.forEach((radio) => {
-    
+
     radio.addEventListener("change", (e) => {
+        let selectedAnswerDiv = document.getElementById(e.target.id).parentNode;
+        console.log('selectedAnswerDiv', selectedAnswerDiv);
+
+        let answerResult = radioMessages[e.target.id].correct;
+        let correctClass = answerResult ? 'correct' : 'incorrect';
+
+        // I cannot figure out how to toggle the classes so I just clear them out - this is meh, ok
+        let allAnswerDivs = document.querySelectorAll("div.answer");
+        allAnswerDivs.forEach((answerDiv) => {
+            answerDiv.classList.remove('correct');
+            answerDiv.classList.remove('incorrect');
+            answerDiv.classList.remove('selected');
+        });
+
+        let messageDivs = document.querySelectorAll(".message");
+        messageDivs.forEach((message) => {
+            message.innerHTML = '';
+        });
+
+        selectedAnswerDiv.classList.add(`${correctClass}`);
+        selectedAnswerDiv.classList.add('selected');
+
         let selectedRadioButtonMessageDiv = document.getElementById(
             `${e.target.id}Message`
         );
-
-        let allMessages = document.querySelectorAll(".message");
-        allMessages.forEach((message) => {
-            message.style.display = "none";
-        });
-
-        let selectedRadioMessage = radioMessages[e.target.id];
-        selectedRadioButtonMessageDiv.style.display = "inline";
-
+        let selectedRadioMessage = radioMessages[e.target.id].message;
         selectedRadioButtonMessageDiv.innerHTML = selectedRadioMessage;
     });
 });
@@ -35,16 +61,16 @@ radios.forEach((radio) => {
 
 
 //Tab Module
-function openCase(event, caseNum){
+function openCase(event, caseNum) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabs content");
-    for (i=0 ; i < tabcontent.length ; i++){
-        tabcontent[i].style.display="none";
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tabs links");
-    for (i=0; i< tablinks.length; i++){
+    for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active"
-        , "");
+            , "");
     }
     document.getElementById(caseNum).style.display = "block";
     event.currentTarget.className += " active";
